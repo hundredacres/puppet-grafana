55b1750153b9a6da3819500fb55742f64aeaa3f7
Matthew Schmitt <matthew.schmitt@riskiq.net>
2016-02-04 19:15:34 -0800

    - Adding dependancy on packagecloud for managing repos

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,10 +1,7 @@
 class grafana::repo {
-  yum::managed_yumrepo { 'grafana':
-    baseurl       => 'https://packagecloud.io/grafana/stable/el/6/$basearch',
-    enabled       => 1,
-    gpgcheck      => 1,
-    gpgkey_source => 'https://packagecloud.io/gpg.key https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana',
-    sslverify     => 1,
-    sslcacert     => '/etc/pki/tls/certs/ca-bundle.crt',
+  include ::packagecloud
+
+  packagecloud::repo { 'grafana/stable':
+    type => 'rpm',  # or "deb" or "gem"
   }
 }
